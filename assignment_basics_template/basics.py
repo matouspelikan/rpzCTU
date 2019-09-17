ccba8a67cf0432fcb592d01b2d2d9427842b7eef
serycjon <jonas.serych@gmail.com>
2019-09-17 16:05:52 +0200

    template update

--- assignment_basics_template/basics.py
+++ assignment_basics_template/basics.py
@@ -24,34 +24,9 @@ def matrix_manip(A, B):
        output.D_select
 
     """
+
     raise NotImplementedError("You have to implement this function.")
-
-    output = {}
-
-    # 1. Find the transpose of the matrix A:
-    output['A_transpose'] = None # TODO
-
-    # 2. Select the third column of the matrix A:
-    output['A_3rd_col'] = None # TODO
-
-    # 3. Select last two rows and last three columns of the matrix A and return the matrix in output.A_slice.
-    output['A_slice'] = None # TODO
-
-    # 4.Find all positions in A greater then 3 and increment them by 1 and add a column of ones to the matrix.
-    # Save the result to matrix A_gr_inc:
-    output['A_gr_inc'] = None # TODO
-
-    # 5. Create matrix C such that Ci,j=∑nk=1A_gr_inci,k⋅A_gr_incTk,j and store it in output.C.
-    output['C'] = None # TODO
-
-    # 6. Compute ∑nc=1c⋅∑mr=1A_gr_incr,c:
-    output['A_weighted_col_sum'] = None # TODO
-
-    # 7. Subtract a vector (4,6)T from all columns of matrix B. Save the result to matrix output.D.
-    output['D'] = None # TODO
-
-    # 8. Select all vectors in the matrix D, which have greater euclidean distance than the average euclidean distance.
-    output['D_select'] = None # TODO
+    output = None
     return output
 
 
@@ -62,14 +37,13 @@ def compute_letter_mean(letter_char, alphabet, images, labels):
     Compute mean image for a letter.
 
     :param letter_char:  character, e.g. 'm'
-    :param alphabet:     list of all characters present in images, e.g. 'abcdefgh'
-    :param images:       images of letters, matrix of size (height x width x number of images) (numpy.array)
-    :param labels:       image labels, vector of size <number of images>; <label> is index to <Alphabet>
-    :return:             mean of all images of the <letter_char>, HxW, rounded and converted to uint8 type
+    :param alphabet:     np.array of all characters present in images <n_letters x >
+    :param images:       images of letters, np.array of size <height x width x number of images>
+    :param labels:       image labels, np.array of size <number of images>; <label> is index to <Alphabet>
+    :return:             mean of all images of the <letter_char>, uint8 type
     """
     raise NotImplementedError("You have to implement this function.")
-
-    letter_mean = None # TODO
+    letter_mean = None
     return letter_mean
 
 
@@ -81,18 +55,59 @@ def compute_lr_histogram(letter_char, alphabet, images, labels, num_bins, return
 
     :param letter_char:                is a character representing the letter whose feature histogram
                                        we want to compute, e.g. 'C'
-    :param alphabet:                   string of characters
+    :param alphabet:                   np.array of all characters present in images <n_letters x >
     :param images:                     images in 3d matrix of shape <h x w x n>
-    :param labels:                     labels of images, indices to Alphabet list, <1 x n>
+    :param labels:                     labels of images, indices to Alphabet list, <n x >
     :param num_bins:                   number of histogram bins
-    :param return_bin_edges:           toggle additional output
-    :return:                           counts of values in the corresponding bins, vector <1 x num_bins>
-    :return:                           (optional if return_bin_edges) vector of bin edges, vector <1 x num_bins + 1>
+    :param return_bin_edges:
+    :return:                           counts of values in the corresponding bins, np.array <num_bins, >,
+                                       bin edges, if return_bin_edges is True, np.array <num_bins+1, >
     """
-    raise NotImplementedError("You have to implement this function.")
 
-    lr_histogram = None # TODO
+    raise NotImplementedError("You have to implement this function.")
     if return_bin_edges:
         return lr_histogram, bin_edges
     else:
         return lr_histogram
+
+
+def histogram_plot(hist_data, color, alpha):
+    """
+    Plot histogram from outputs of compute_lr_histogram
+
+    :param hist_data: tuple of (histogram values, histogram bin edges)
+    :param color:     color of the histogram (passed to matplotlib)
+    :param alpha:     transparency alpha of the histogram (passed to matplotlib)
+    """
+
+    raise NotImplementedError("You have to implement this function.")
+
+################################################################################
+#####                                                                      #####
+#####             Below this line are already prepared methods             #####
+#####                                                                      #####
+################################################################################
+
+def montage(images, colormap='gray'):
+    """
+    Show images in grid.
+
+    :param images:  np.array <h x w x n>
+    """
+    h, w, count = np.shape(images)
+    h_sq = np.int(np.ceil(np.sqrt(count)))
+    w_sq = h_sq
+    im_matrix = np.zeros((h_sq * h, w_sq * w))
+
+    image_id = 0
+    for k in range(w_sq):
+        for j in range(h_sq):
+            if image_id >= count:
+                break
+            slice_w = j * h
+            slice_h = k * w
+            im_matrix[slice_h:slice_h + w, slice_w:slice_w + h] = images[:, :, image_id]
+            image_id += 1
+    plt.imshow(im_matrix, cmap=colormap)
+    plt.axis('off')
+    return im_matrix
