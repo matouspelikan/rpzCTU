79ae84a36a3c844d50fd10d2dfd2bdf755105464
serycjon <jonas.serych@gmail.com>
2019-09-19 12:54:08 +0200

    migrate Bayes to python

--- assignment_bayes_template/bayes.py
+++ assignment_bayes_template/bayes.py
@@ -8,21 +8,8 @@ import scipy.io
 import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
-matplotlib.rcParams['text.usetex'] = True
-matplotlib.rcParams['text.latex.unicode'] = True
 from scipy.stats import norm
 from PIL import Image
-
-
-def unwrap(data):
-    try:
-        while (len(data) == 1) and (len(data.shape) > 0):
-            data = data[0]
-    except TypeError:
-        pass
-    except:
-        pass
-    return data
 
 
 def bayes_risk_discrete(discrete_A, discrete_B, W, q):
@@ -31,17 +18,15 @@ def bayes_risk_discrete(discrete_A, discrete_B, W, q):
 
     Compute bayesian risk for a discrete strategy q
 
-    :param discrete_A['Prob']:      pXk(x|A) given as a <1 × n> np array
+    :param discrete_A['Prob']:      pXk(x|A) given as a (n, ) np.array
     :param discrete_A['Prior']:     prior probability pK(A)
-    :param discrete_B['Prob']:      pXk(x|B) given as a <1 × n> np array
+    :param discrete_B['Prob']:      pXk(x|B) given as a (n, ) np.array
     :param discrete_B['Prior']:     prior probability pK(B)
-    :param W:                       cost function matrix
-                                    dims: <states x decisions>
+    :param W:                       cost function np.array (states, decisions)
                                     (nr. of states and decisions is fixed to 2)
-    :param q:                       strategy - <1 × n> np array, values 0 or 1
-    :return:                        bayesian risk, <1 x 1>
+    :param q:                       strategy - (n, ) np.array, values 0 or 1
+    :return:                        bayesian risk - scalar
     """
-
     raise NotImplementedError("You have to implement this function.")
     R = None
     return R
@@ -53,19 +38,16 @@ def find_strategy_discrete(discrete_A, discrete_B, W):
 
     Find bayesian strategy for 2 discrete distributions.
 
-    :param discrete_A['Prob']:      pXk(x|A) given as a <1 × n> np array
+    :param discrete_A['Prob']:      pXk(x|A) given as a (n, ) np.array
     :param discrete_A['Prior']:     prior probability pK(A)
-    :param discrete_B['Prob']:      pXk(x|B) given as a <1 × n> np array
+    :param discrete_B['Prob']:      pXk(x|B) given as a (n, ) np.array
     :param discrete_B['Prior']:     prior probability pK(B)
-    :param W:                       cost function matrix
-                                    dims: <states x decisions>
+    :param W:                       cost function np.array (states, decisions)
                                     (nr. of states and decisions is fixed to 2)
-    :return:                        q - optimal strategy <1 x n>
+    :return:                        q - optimal strategy (n, ) np.array
     """
-
     raise NotImplementedError("You have to implement this function.")
     q = None
-
     return q
 
 
@@ -75,12 +57,13 @@ def classify_discrete(imgs, q):
 
     Classify images using discrete measurement and strategy q.
 
-    :param imgs:    test set images, <h x w x n>
-    :param q:       strategy <1 × 21> np array of 0 or 1
-    :return:        image labels, <1 x n>
+    :param imgs:    test set images, (h, w, n) np.array
+    :param q:       strategy (21, ) np.array of 0 or 1
+    :return:        image labels, (n, ) np.array of 0 or 1
     """
     raise NotImplementedError("You have to implement this function.")
     label = None
+
     return label
 
 
@@ -90,9 +73,9 @@ def classification_error_discrete(images, labels, q):
 
     Compute classification error for a discrete strategy q.
 
-    :param images:      <h x w x n> np array
-    :param labels:      <1 x n> np array of values 0 or 1
-    :param q:           <1 × m> np array of 0 or 1
+    :param images:      images, (h, w, n) np.array
+    :param labels:      (n, ) np.array of values 0 or 1
+    :param q:           (m, ) np.array of 0 or 1
     :return:            error - classification error as a fraction of false samples
                         scalar in range <0, 1>
     """
@@ -111,12 +94,13 @@ def find_strategy_2normal(distribution_A, distribution_B):
                             distribution_A['Mean'], distribution_A['Sigma'], distribution_A['Prior']
     :param distribution_B:  the same as distribution_A
     :return q:              strategy
-%               q['t1'] q['t2'] - two descision thresholds
-%               q['decision'] - 3 decisions for intervals (-inf, t1>, (t1, t2>, (t2, inf)
-%                            shape <1 x 3>
+                               q['t1'], q['t2'] - descision thresholds
+                               q['decision'] - (3, ) np.int32 np.array decisions for intervals (-inf, t1>, (t1, t2>, (t2, inf)
+
     """
     raise NotImplementedError("You have to implement this function.")
     q = None
+
     return q
 
 
@@ -130,10 +114,9 @@ def bayes_risk_2normal(distribution_A, distribution_B, q):
                             distribution_A['Mean'], distribution_A['Sigma'], distribution_A['Prior']
     :param distribution_B:  the same as distribution_A
     :param q:               strategy
-%                           q['t1'] q['t2'] - two descision thresholds
-%                           q['decision'] - 3 decisions for intervals (-inf, t1>, (t1, t2>, (t2, inf)
-%                           shape <1 x 3>
-    :return:    R - bayesian risk, scalar
+                               q['t1'], q['t2'] - float descision thresholds
+                               q['decision'] - (3, ) np.int32 np.array decisions for intervals (-inf, t1>, (t1, t2>, (t2, inf)
+    :return:    R - bayesian risk, float
     """
     raise NotImplementedError("You have to implement this function.")
     R = None
@@ -146,12 +129,11 @@ def classify_2normal(imgs, q):
 
     Classify images using continuous measurement and strategy q.
 
-    :param imgs:    test set images, <h x w x n>
+    :param imgs:    test set images, np.array (h, w, n)
     :param q:       strategy
-%                   q['t1'] q['t2'] - two descision thresholds
-%                   q['decision'] - 3 decisions for intervals (-inf, t1>, (t1, t2>, (t2, inf)
-%                   shape <1 x 3>
-    :return:        label - image labels, <1 x n>
+                    q['t1'] q['t2'] - float descision thresholds
+                    q['decision'] - (3, ) int32 np.array decisions for intervals (-inf, t1>, (t1, t2>, (t2, inf)
+    :return:        label - image labels, (n, ) int32
     """
     raise NotImplementedError("You have to implement this function.")
     label = None
@@ -164,13 +146,12 @@ def classification_error_2normal(images, labels, q):
 
     Compute classification error of a strategy q in a test set.
 
-    :param images:  test set images, <h x w x n>
-    :param labels:  test set labels <1 x n>
+    :param images:  test set images, (h, w, n)
+    :param labels:  test set labels (n, )
     :param q:       strategy
-%                   q['t1'] q['t2'] - two descision thresholds
-%                   q['decision'] - 3 decisions for intervals (-inf, t1>, (t1, t2>, (t2, inf)
-%                   shape <1 x 3>
-    :return:        classification error in range <0, 1>
+                    q['t1'] q['t2'] - float descision thresholds
+                    q['decision'] - (3, ) np.int32 decisions for intervals (-inf, t1>, (t1, t2>, (t2, inf)
+    :return:        float classification error in range <0, 1>
     """
     raise NotImplementedError("You have to implement this function.")
     error = None
@@ -192,16 +173,17 @@ def compute_measurement_lr_cont(imgs):
     Compute measurement on images, subtract sum of right half from sum of
     left half.
 
-    :param imgs:    set of images, <h x w x n>
-    :return:        measurements, <1 x n>
+    :param imgs:    set of images, (h, w, n)
+    :return:        measurements, (n, )
     """
+    assert len(imgs.shape) == 3
 
     width = imgs.shape[1]
     sum_rows = np.sum(imgs, dtype=np.float64, axis=0)
 
-    x = np.sum(sum_rows[0:int(width / 2),:],axis=0) - np.sum(sum_rows[int(width / 2):,:], axis=0)
-    x = np.atleast_2d(x)
-    # x = reshape(x,1,size(imgs,3));
+    x = np.sum(sum_rows[0:int(width / 2),:], axis=0) - np.sum(sum_rows[int(width / 2):,:], axis=0)
+
+    assert x.shape == (imgs.shape[2], )
     return x
 
 
@@ -211,12 +193,11 @@ def visualize_discrete(discrete_A, discrete_B, q):
 
     Visualize a strategy for 2 discrete distributions.
 
-    :param discrete_A['Prob']:      pXk(x|A) given as a <1 × n> np array
+    :param discrete_A['Prob']:      pXk(x|A) given as a (n, ) np.array
     :param discrete_A['Prior']:     prior probability pK(A)
-    :param discrete_B['Prob']:      pXk(x|B) given as a <1 × n> np array
+    :param discrete_B['Prob']:      pXk(x|B) given as a (n, ) np.array
     :param discrete_B['Prior']:     prior probability pK(B)
-    :param q:                       strategy - <1 × n> np array, values 1 or 2
-    :return:
+    :param q:                       strategy - (n, ) np.array, values 0 or 1
     """
 
     posterior_A = discrete_A['Prob'] * discrete_A['Prior']
@@ -233,8 +214,8 @@ def visualize_discrete(discrete_A, discrete_B, q):
     bins = np.array(range(posterior_A.size + 1)) - int(posterior_A.size / 2)
 
     width = 0.75
-    bar_plot_A = plt.bar(bins[:-1], posterior_A[0], width=width, color='b', alpha=0.75)
-    bar_plot_B = plt.bar(bins[:-1], posterior_B[0], width=width, color='r', alpha=0.75)
+    bar_plot_A = plt.bar(bins[:-1], posterior_A, width=width, color='b', alpha=0.75)
+    bar_plot_B = plt.bar(bins[:-1], posterior_B, width=width, color='r', alpha=0.75)
 
     plt.legend((bar_plot_A, bar_plot_B), (r'$p_{XK}(x,A)$', r'$p_{XK}(x,B)$'))
 
@@ -242,13 +223,63 @@ def visualize_discrete(discrete_A, discrete_B, q):
     height = np.abs(sub_level)
     for idx in range(len(bins[:-1])):
         b = bins[idx]
-        col = 'r' if q[0, idx] == 1 else 'b'
+        col = 'r' if q[idx] == 1 else 'b'
         patch = patches.Rectangle([b - 0.5, sub_level], 1, height, angle=0.0, color=col, alpha=0.75)
         ax.add_patch(patch)
 
     plt.ylim(bottom=sub_level)
     plt.text(bins[0], -max_prob / 16, 'strategy q')
 
+def visualize_2norm(cont_A, cont_B, q):
+    n_sigmas = 5
+    n_points = 200
+
+    A_range = (cont_A['Mean'] - n_sigmas * cont_A['Sigma'],
+               cont_A['Mean'] + n_sigmas * cont_A['Sigma'])
+    B_range = (cont_B['Mean'] - n_sigmas * cont_B['Sigma'],
+               cont_B['Mean'] + n_sigmas * cont_B['Sigma'])
+    start = min(A_range[0], B_range[0])
+    stop = max(A_range[1], B_range[1])
+
+    xs = np.linspace(start, stop, n_points)
+    A_vals = cont_A['Prior'] * norm.pdf(xs, cont_A['Mean'], cont_A['Sigma'])
+    B_vals = cont_B['Prior'] * norm.pdf(xs, cont_B['Mean'], cont_B['Sigma'])
+
+    colors = ['r', 'b']
+    plt.plot(xs, A_vals, c=colors[0], label='A')
+    plt.plot(xs, B_vals, c=colors[1], label='B')
+
+    plt.axvline(x=q['t1'], c='k', lw=0.5, ls=':')
+    plt.axvline(x=q['t2'], c='k', lw=0.5, ls=':')
+
+    offset = 0.000007
+    sub_level = -0.000025
+    left = xs[0]
+    right = xs[-1]
+    def clip(x, lb, ub):
+        res = x
+        if res < lb:
+            res = lb
+        if res > ub:
+            res = ub
+        return res
+    t1 = clip(q['t1'], xs[0], xs[-1])
+    t2 = clip(q['t2'], xs[0], xs[-1])
+
+    patch = patches.Rectangle([left, sub_level], t1-left, -sub_level-offset, angle=0.0,
+                              color=colors[q['decision'][0]], alpha=0.75)
+    plt.gca().add_patch(patch)
+    patch = patches.Rectangle([t1, sub_level], t2-t1, -sub_level-offset, angle=0.0,
+                              color=colors[q['decision'][1]], alpha=0.75)
+    plt.gca().add_patch(patch)
+    patch = patches.Rectangle([t2, sub_level], right-t2, -sub_level-offset, angle=0.0,
+                              color=colors[q['decision'][2]], alpha=0.75)
+    plt.gca().add_patch(patch)
+    plt.legend()
+
+    plt.title("Posterior probabilities and strategy q")
+    plt.xlabel("image LR feature")
+    plt.ylabel("posterior probabilities")
 
 def compute_measurement_lr_discrete(imgs):
     """
@@ -256,10 +287,12 @@ def compute_measurement_lr_discrete(imgs):
 
     Calculates difference between left and right half of image(s).
 
-    :param imgs:    set of images, <h x w x n> or <h x w x 3 x n> (color images)
-    :return:        np array of values in range <-10, 10>,
-                    shape <1 x n>
+    :param imgs:    set of images, (h, w, n) (or for color images (h, w, 3, n)) np.array
+    :return:        (n, ) np.array of values in range <-10, 10>,
     """
+    assert len(imgs.shape) in (3, 4)
+    assert (imgs.shape[2] == 3 or len(imgs.shape) == 3)
+
     mu = -563.9
     sigma = 2001.6
 
@@ -277,4 +310,29 @@ def compute_measurement_lr_discrete(imgs):
     x[x > 10] = 10
     x[x < -10] = -10
 
+    assert x.shape == (imgs.shape[-1], )
     return x
+
+def montage(images, colormap='gray'):
+    """
+    Show images in grid.
+
+    :param images:  np.array (h, w, n)
+    """
+    h, w, count = np.shape(images)
+    h_sq = np.int(np.ceil(np.sqrt(count)))
+    w_sq = h_sq
+    im_matrix = np.zeros((h_sq * h, w_sq * w))
+
+    image_id = 0
+    for k in range(w_sq):
+        for j in range(h_sq):
+            if image_id >= count:
+                break
+            slice_w = j * h
+            slice_h = k * w
+            im_matrix[slice_h:slice_h + w, slice_w:slice_w + h] = images[:, :, image_id]
+            image_id += 1
+    plt.imshow(im_matrix, cmap=colormap)
+    plt.axis('off')
+    return im_matrix
