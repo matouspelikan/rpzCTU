8ca19e10c499c2ea8e2a85b9cc35327e4da07fe4
serycjon <jonas.serych@gmail.com>
2019-10-10 16:52:48 +0200

    add mle_map_bayes

---
+++ assignment_mle_map_bayes_template/mle_map_bayes.py
@@ -0,0 +1,245 @@
+import numpy as np
+from scipy.stats import norm
+import scipy.special as spec  # for gamma
+
+# MLE
+def ml_estim_normal(x):
+    """
+    Computes maximum likelihood estimate of mean and variance of a normal distribution.
+
+    :param x:   measurements (n, )
+    :return:    mu - mean - python float
+                var - variance - python float
+    """
+    raise NotImplementedError("You have to implement this function.")
+
+    return mu, var
+
+
+def ml_estim_categorical(counts):
+    """
+    Computes maximum likelihood estimate of categorical distribution parameters.
+
+    :param counts: measured bin counts (n, )
+    :return:       pk - (n, ) parameters of the categorical distribution
+    """
+    raise NotImplementedError("You have to implement this function.")
+    return pk
+
+# MAP
+def map_estim_normal(x, mu0, nu, alpha, beta):
+    """
+    Maximum a posteriori parameter estimation of normal distribution with normal inverse gamma prior.
+
+    :param x:      measurements (n, )
+    :param mu0:    NIG parameter - python float
+    :param nu:     NIG parameter - python float
+    :param alpha:  NIG parameter - python float
+    :param beta:   NIG parameter - python float
+
+    :return:       mu - estimated mean - python float
+    :return:       var - estimated variance - python float
+    """
+    raise NotImplementedError("You have to implement this function.")
+
+    return mu, var
+
+
+def map_estim_categorical(counts, alpha):
+    """
+    Maximum a posteriori parameter estimation of categorical distribution with Dirichlet prior.
+
+    :param counts:  measured bin counts (n, )
+    :param alpha:   Dirichlet distribution parameters (n, )
+
+    :return:        pk - estimated categorical distribution parameters (n, )
+    """
+    raise NotImplementedError("You have to implement this function.")
+
+    return pk
+
+# BAYES
+def bayes_posterior_params_normal(x, prior_mu0, prior_nu, prior_alpha, prior_beta):
+    """
+    Compute a posteriori normal inverse gamma parameters from data and NIG prior.
+
+    :param x:            measurements (n, )
+    :param prior_mu0:    NIG parameter - python float
+    :param prior_nu:     NIG parameter - python float
+    :param prior_alpha:  NIG parameter - python float
+    :param prior_beta:   NIG parameter - python float
+
+    :return:             mu0:    a posteriori NIG parameter - python float
+    :return:             nu:     a posteriori NIG parameter - python float
+    :return:             alpha:  a posteriori NIG parameter - python float
+    :return:             beta:   a posteriori NIG parameter - python float
+    """
+    raise NotImplementedError("You have to implement this function.")
+
+    return mu0, nu, alpha, beta
+
+def bayes_posterior_params_categorical(counts, alphas):
+    """
+    Compute a posteriori Dirichlet parameters from data and Dirichlet prior.
+
+    :param counts:   measured bin counts (n, )
+    :param alphas:   prior Dirichlet distribution parameters (n, )
+
+    :return:         posterior_alphas - estimated Dirichlet distribution parameters (n, )
+    """
+    raise NotImplementedError("You have to implement this function.")
+    return posterior_alphas
+
+def bayes_estim_pdf_normal(x_test, x,
+                           mu0, nu, alpha, beta):
+    """
+    Compute pdf of predictive distribution for Bayesian estimate for normal distribution with normal inverse gamma prior.
+
+    :param x_test:  values where the pdf should be evaluated (m, )
+    :param x:       'training' measurements (n, )
+    :param mu0:     prior NIG parameter - python float
+    :param nu:      prior NIG parameter - python float
+    :param alpha:   prior NIG parameter - python float
+    :param beta:    prior NIG parameter - python float
+
+    :return:        pdf - Bayesian estimate pdf evaluated at x_test (m, )
+    """
+    raise NotImplementedError("You have to implement this function.")
+
+    return pdf
+
+def bayes_estim_categorical(counts, alphas):
+    """
+    Compute parameters of Bayesian estimate for categorical distribution with Dirichlet prior.
+
+    :param counts:  measured bin counts (n, )
+    :param alphas:  prior Dirichlet distribution parameters (n, )
+
+    :return:        pk - estimated categorical distribution parameters (n, )
+    """
+    raise NotImplementedError("You have to implement this function.")
+
+    return pk
+
+# Classification
+def mle_Bayes_classif(test_imgs, train_data_A, train_data_C):
+    """
+    Classify images using Bayes classification using MLE of normal distributions and 0-1 loss.
+
+    :param test_imgs:      images to be classified (H, W, N)
+    :param train_data_A:   training image features A (nA, )
+    :param train_data_C:   training image features C (nC, )
+
+    :return:               q - classification strategy (see find_strategy_2normal)
+    :return:               labels - classification of test_imgs (N, ) 0 or 1
+    :return:               DA - parameters of the normal distribution of A
+                            DA['Mean'] - python float
+                            DA['Sigma'] - python float
+                            DA['Prior'] - python float
+    :return:               DC - parameters of the normal distribution of C
+                            DC['Mean'] - python float
+                            DC['Sigma'] - python float
+                            DC['Prior'] - python float
+    """
+    raise NotImplementedError("You have to implement this function.")
+
+    return q, labels, DA, DC
+
+
+def map_Bayes_classif(test_imgs, train_data_A, train_data_C,
+                      mu0_A, nu_A, alpha_A, beta_A,
+                      mu0_C, nu_C, alpha_C, beta_C):
+    """
+    Classify images using Bayes classification using MAP estimate of normal distributions with NIG priors and 0-1 loss.
+
+    :param test_imgs:      images to be classified (H, W, N)
+    :param train_data_A:   training image features A (nA, )
+    :param train_data_C:   training image features C (nC, )
+
+    :param mu0_A:          prior NIG parameter for A - python float
+    :param nu_A:           prior NIG parameter for A - python float
+    :param alpha_A:        prior NIG parameter for A - python float
+    :param beta_A:         prior NIG parameter for A - python float
+
+    :param mu0_C:          prior NIG parameter for C - python float
+    :param nu_C:           prior NIG parameter for C - python float
+    :param alpha_C:        prior NIG parameter for C - python float
+    :param beta_C:         prior NIG parameter for C - python float
+
+    :return:               q - classification strategy (see find_strategy_2normal)
+    :return:               labels - classification of test_imgs (N, ) 0 or 1
+    :return:               DA - parameters of the normal distribution of A
+                            DA['Mean'] - python float
+                            DA['Sigma'] - python float
+                            DA['Prior'] - python float
+    :return:               DC - parameters of the normal distribution of C
+                            DC['Mean'] - python float
+                            DC['Sigma'] - python float
+                            DC['Prior'] - python float
+    """
+    raise NotImplementedError("You have to implement this function.")
+
+    return q, labels, DA, DC
+
+
+def bayes_Bayes_classif(x_test, x_train_A, x_train_C,
+                        mu0_A, nu_A, alpha_A, beta_A,
+                        mu0_C, nu_C, alpha_C, beta_C):
+    """
+    Classify images using Bayes classification (0-1 loss) using predictive pdf estimated using Bayesian inferece with with NIG priors.
+
+    :param x_test:         images features to be classified (n, )
+    :param x_train_A:      training image features A (nA, )
+    :param x_train_C:      training image features C (nC, )
+
+    :param mu0_A:          prior NIG parameter for A - python float
+    :param nu_A:           prior NIG parameter for A - python float
+    :param alpha_A:        prior NIG parameter for A - python float
+    :param beta_A:         prior NIG parameter for A - python float
+
+    :param mu0_C:          prior NIG parameter for C - python float
+    :param nu_C:           prior NIG parameter for C - python float
+    :param alpha_C:        prior NIG parameter for C - python float
+    :param beta_C:         prior NIG parameter for C - python float
+
+    :return:               labels - classification of x_test (n, ) 0 or 1
+    """
+    raise NotImplementedError("You have to implement this function.")
+
+    return labels
+
+#### Previous labs here:
+
+
+#### provided functions
+
+def mle_likelihood_normal(x, mu, var):
+    """
+    Compute the likelihood of the data x given the model is a normal distribution with given mean and sigma
+
+    :param x:       measurements (n, )
+    :param mu:      the normal distribution mean
+    :param var:     the normal distribution variance
+    :return:        L - likelihood of the data x
+    """
+    assert len(x.shape) == 1
+
+    if var <= 0:
+        L = 0
+    else:
+        L = np.prod(norm.pdf(x, mu, np.sqrt(var)))
+    return L
+
+def norm_inv_gamma_pdf(mu, var, mu0, nu, alpha, beta):
+    # Wikipedia sometimes uses a symbol 'lambda' instead 'nu'
+
+    assert alpha > 0
+    assert nu > 0
+    if beta <= 0 or var <= 0:
+        return 0
+
+    sigma = np.sqrt(var)
+
+    p = np.sqrt(nu) / (sigma * np.sqrt(2 * np.pi)) * np.float_power(beta, alpha) / spec.gamma(alpha) * np.float_power(1/var, alpha + 1) * np.exp(-(2 * beta + nu * (mu0 - mu) * (mu0 - mu)) / (2 * var))
+
+    return p
