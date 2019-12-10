e655e439d800c7dc2a0c1f3d8cad368e1ed75416
serycjon <jonas.serych@gmail.com>
2019-12-10 13:15:43 +0100

    update for 2019

--- assignment_kmeans_template/kmeans.py
+++ assignment_kmeans_template/kmeans.py
@@ -3,173 +3,128 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-import random
 import itertools
+
 
 def k_means(x, k, max_iter, show=False, init_means=None):
     """
-    c, means, sq_dists = k_means(x, k, max_iter, show, init_means)
-
     Implementation of the k-means clustering algorithm.
 
-    :param x:           Feature vectors, np.array of size <dim x number_of_vectors> (float64/double),
-                        where dim is arbitrary feature vector dimension
-    :param k:           Required number of clusters (scalar, int32)
-    :param max_iter:    Stopping criterion: max. number of iterations (scalar, int32);
-                        Set it to infinity (np.inf) if you wish to deactivate this criterion
-    :param show:        Boolean switch to turn on/off visualization of partial
-                        results, optional.
-    :param init_means:  Initial cluster prototypes, np array <dim x k> (float64/double), optional
-    :return:        c - Cluster index for each feature vector, np array of size
-                        <1 x number_of_vectors>, containing only values from 1 to k,
-                        i.e. c[i] is the index of a cluster which the vector x[:,i]
-                        belongs to.
-                means - Cluster centers, np array of size <dim x k> (float64/double),
-                        i.e. means[:,i] is the center of the i-th cluster.
-             sq_dists - Squared distances to the nearest mean for each feature vector,
-                        np array of size <1 x number_of_vectors> (float64/double)
+    :param x:               feature vectors, np array (dim, number_of_vectors)
+    :param k:               required number of clusters, scalar
+    :param max_iter:        stopping criterion: max. number of iterations
+    :param show:            (optional) boolean switch to turn on/off visualization of partial results
+    :param init_means:      (optional) initial cluster prototypes, np array (dim, k)
+
+    :return cluster_labels: cluster index for each feature vector, np array (number_of_vectors, )
+                            array contains only values from 0 to k-1,
+                            i.e. cluster_labels[i] is the index of a cluster which the vector x[:,i] belongs to.
+    :return centroids:      cluster centroids, np array (dim, k), same type as x
+                            i.e. centroids[:,i] is the center of the i-th cluster.
+    :return sq_dists:       squared distances to the nearest centroid for each feature vector,
+                            np array (number_of_vectors, )
 
     Note 1: The iterative procedure terminates if either maximum number of iterations is reached
-          or there is no change in assignment of data to the clusters.
+            or there is no change in assignment of data to the clusters.
 
     Note 2: DO NOT MODIFY INITIALIZATIONS
 
     """
-
     # Number of vectors
-    N = x.shape[1]
-    c = np.zeros([1, N], np.int32)
+    n_vectors = x.shape[1]
+    cluster_labels = np.zeros([n_vectors], np.int32)
 
     # Means initialization
     if init_means is None:
-        ind = random.sample(range(N), k)
-        means = x[:, ind]
+        ind = np.random.choice(n_vectors, k, replace=False)
+        centroids = x[:, ind]
     else:
-        means = init_means
+        centroids = init_means
 
     i_iter = 0
     while i_iter < max_iter:
-
-        # YOUR CODE HERE
         raise NotImplementedError("You have to implement this function.")
-
         # Ploting partial results
         if show:
             print('Iteration: {:d}'.format(i_iter))
-            show_clusters(x, c, means)
+            show_clusters(x, cluster_labels, centroids, title='Iteration: {:d}'.format(i_iter))
 
-        if show:
-            print('Done.')
+    if show:
+        print('Done.')
 
-    return c, means, sq_dists
+    raise NotImplementedError("You have to implement this function.")
+    cluster_labels, centroids, sq_dists = None, None, None
+    return cluster_labels, centroids, sq_dists
 
 
 def k_means_multiple_trials(x, k, n_trials, max_iter, show=False):
     """
-    c, means, sq_dists = k_means_multiple_trials(x, k, n_trials, max_iter, show)
-
-    Performs several trials of the k-means clustering algorithm in order to
+    Performs several trials of the k-centroids clustering algorithm in order to
     avoid local minima. Result of the trial with the lowest "within-cluster
     sum of squares" is selected as the best one and returned.
 
-    :param x:           Feature vectors, np.array of size <dim x number_of_vectors> (float64/double),
-                        where dim is arbitrary feature vector dimension
-    :param k:           Required number of clusters (scalar, int32)
-    :param n_trials:    Number of trials.
-    :param max_iter:    Stopping criterion: max. number of iterations (scalar, int32);
-                        Set it to infinity (np.inf) if you wish to deactivate this criterion
-    :param show:        Boolean switch to turn on/off visualization of partial
-                        results, optional.
-    :return: (= information about the best clustering from all the trials):
-                    c - Cluster index for each feature vector, np array of size
-                        <1 x number_of_vectors>, containing only values from 1 to k,
-                        i.e. c[i] is the index of a cluster which the vector x[:,i]
-                        belongs to.
-                means - Cluster centers, np array of size <dim x k> (float64/double),
-                        i.e. means[:,i] is the center of the i-th cluster.
-             sq_dists - Squared distances to the nearest mean for each feature vector,
-                        np array of size <1 x number_of_vectors> (float64/double)
+    :param x:               feature vectors, np array (dim, number_of_vectors)
+    :param k:               required number of clusters, scalar
+    :param n_trials:        number of trials, scalars
+    :param max_iter:        stopping criterion: max. number of iterations
+    :param show:            (optional) boolean switch to turn on/off visualization of partial results
+
+    :return cluster_labels: cluster index for each feature vector, np array (number_of_vectors, ),
+                            array contains only values from 0 to k-1,
+                            i.e. cluster_labels[i] is the index of a cluster which the vector x[:,i] belongs to.
+    :return centroids:      cluster centroids, np array (dim, k), same type as x
+                            i.e. centroids[:,i] is the center of the i-th cluster.
+    :return sq_dists:       squared distances to the nearest centroid for each feature vector,
+                            np array (number_of_vectors, )
     """
-
-
-    # YOUR CODE HERE
-
-    # Multiple trial of the k-means clustering algorithm
-    for i_trial in range(n_trials):
-
-        # YOUR CODE HERE
-
-        # Plotting partial results
-        if show:
-            # YOUR CODE HERE (use function show_clusters)
-            raise NotImplementedError("You have to implement this function.")
-
     raise NotImplementedError("You have to implement this function.")
-    return c, means, sq_dists
+    cluster_labels, centroids, sq_dists = None, None, None
+    return cluster_labels, centroids, sq_dists
 
 
 def random_sample(weights):
     """
-    idx = random_sample(weights)
-
     picks randomly a sample based on the sample weights.
-    Suppose weights / sum(weights) is a discrete probability distribution.
 
-    :param weights: array of sample weights <1 x n> (float64/double)
-    :return:        idx - index of chosen sample (scalar, int32)
+    :param weights: array of sample weights, np array (n, )
+    :return idx:    index of chosen sample, scalar
 
     Note: use np.random.uniform() for random number generation in open interval (0, 1)
     """
-
-    # use np.random.uniform() for random number generation in open interval (0, 1)
-
     raise NotImplementedError("You have to implement this function.")
+    idx = None
     return idx
 
 
 def k_meanspp(x, k):
     """
-    centers = k_meanspp(x, k)
+    performs k-means++ initialization for k-means clustering.
 
-    perform k-means++ initialization for k-means clustering.
+    :param x:           Feature vectors, np array (dim, number_of_vectors)
+    :param k:           Required number of clusters, scalar
 
-    :param x:   Feature vectors, np.array of size <dim x number_of_vectors> (float64/double),
-                where dim is arbitrary feature vector dimension
-    :param k:   Required number of clusters (scalar, int32)
-    :return:    centers - k proposed centers for k-means initialization, np.array <dim x k> (float64/double)
+    :return centroids:  proposed centroids for k-means initialization, np array (dim, k)
     """
-
-    N = x.shape[1]
-
     raise NotImplementedError("You have to implement this function.")
-    return centers
+    centroids = None
+    return centroids
 
 
 def quantize_colors(im, k):
     """
-    im_q = quantize_colors(im, k)
+    Image color quantization using the k-means clustering. A subset of 1000 pixels
+    is first clustered into k clusters based on their RGB color.
+    Quantized image is constructed by replacing each pixel color by its cluster centroid.
 
-    Image color quantization using the k-means clustering. The pixel colors
-    are at first clustered into k clusters. Color of each pixel is then set
-    to the mean color of the cluster to which it belongs to.
-
-    :param im:  Image whose pixel colors are to be quantized, np.array <h x w x 3> (uint8)
-    :param k:   Required number of quantized colors (scalar, int32)
-    :return:    Image with quantized colors, np.array <h x w x 3> (uint8)
+    :param im:          image for quantization, np array (h, w, 3) (np.uint8)
+    :param k:           required number of quantized colors, scalar
+    :return im_q:       image with quantized colors, np array (h, w, 3) (uint8)
+    
+    note: make sure that the k-means is run on floating point inputs.
     """
-
-    # RGB -> LAB conversion is skipped due to server side missing module
-    # Finish this task using rgb image
-
-    # YOUR CODE HERE
-
     raise NotImplementedError("You have to implement this function.")
-
-    # LAB -> RGB conversion is skipped due to server side missing module
-
-    im_q = im_q.astype(dtype=np.uint8)
+    im_q = None
     return im_q
-
 
 
 ################################################################################
@@ -181,14 +136,10 @@ def quantize_colors(im, k):
 
 def compute_measurements(images):
     """
-    X = compute_measurements(data)
-
     computes 2D features from image measurements
 
-    :param data:          dict with keys 'images' and 'labels'
-        - data['images'] - <H x W x N_images> np.uint8 array of images
-
-    :return:              X - <2 x N_images> np.array of features
+    :param images: array of images, np array (H, W, N_images) (np.uint8)
+    :return x:     array of features, np array (2, N_images)
     """
 
     images = images.astype(np.float64)
@@ -207,51 +158,50 @@ def compute_measurements(images):
     a = L - R
     b = U - D
 
-    X = np.vstack((a, b))
-    return X
+    x = np.vstack((a, b))
+    return x
 
 
-def show_clusters(x, c, means):
+def show_clusters(x, cluster_labels, centroids, title=None):
     """
-    show_clusters(x, c, means)
-
     Create plot of feature vectors with same colour for members of same cluster.
 
-    :param x:       Feature vectors, np.array of size <dim x number_of_vectors> (float64/double),
-                    where dim is arbitrary feature vector dimension (show only first 2 dimensions)
-    :param c:       Cluster index for each feature vector, np array of size
-                    <1 x number_of_vectors>, containing only values from 1 to k,
-                    i.e. c[i] is the index of a cluster which the vector x[:,i]
-                    belongs to.
-    :param means:   Cluster centers, np array of size <dim x k> (float64/double),
-                    i.e. means[:,i] is the center of the i-th cluster.
+    :param x:               feature vectors, np array (dim, number_of_vectors) (float64/double),
+                            where dim is arbitrary feature vector dimension
+    :param cluster_labels:  cluster index for each feature vector, np array (number_of_vectors, ),
+                            array contains only values from 1 to k,
+                            i.e. cluster_labels[i] is the index of a cluster which the vector x[:,i] belongs to.
+    :param centroids:       cluster centers, np array (dim, k) (float64/double),
+                            i.e. centroids[:,i] is the center of the i-th cluster.
     """
 
-    c = c.flatten()
-    clusters = np.unique(c)
+    cluster_labels = cluster_labels.flatten()
+    clusters = np.unique(cluster_labels)
     markers = itertools.cycle(['*','o','+','x','v','^','<','>'])
 
-    plt.figure()
+    plt.figure(figsize=(8,7))
     for i in clusters:
-        cluster_x = x[:, c == i]
+        cluster_x = x[:, cluster_labels == i]
         # print(cluster_x)
-        plt.plot(cluster_x[0], cluster_x[1], markers.next())
+        plt.plot(cluster_x[0], cluster_x[1], next(markers))
     plt.axis('equal')
 
-
-    len = means.shape[1]
+    len = centroids.shape[1]
     for i in range(len):
-        plt.plot(means[0,i], means[1,i], 'm+', ms=10, mew=2)
+        plt.plot(centroids[0, i], centroids[1, i], 'm+', ms=10, mew=2)
+
+    plt.axis('equal')
+    plt.grid('on')
+    if title is not None:
+        plt.title(title)
 
 
-def show_clustered_images(images, labels):
+def show_clustered_images(images, labels, title=None):
     """
-    show_clustered_images(test_images, labels[, letters])
-
     Shows results of clustering. Create montages of images according to estimated labels
 
-    :param images:          input images, np.array <h x w x n>
-    :param labels:          labels of input images, np.array <1 x n>
+    :param images:          input images, np array (h, w, n)
+    :param labels:          labels of input images, np array (n, )
     """
     assert (len(images.shape) == 3)
 
@@ -280,7 +230,7 @@ def show_clustered_images(images, labels):
 
     unique_labels = np.unique(labels).flatten()
 
-    plt.figure(figsize=(10,10))
+    fig = plt.figure(figsize=(10,10))
     ww = np.ceil(float(n) / np.sqrt(n))
     hh = np.ceil(float(n) / ww)
 
@@ -289,16 +239,19 @@ def show_clustered_images(images, labels):
         subfig = plt.subplot(hh,ww,i+1)
         montage(imgs)
 
+    if title is not None:
+        fig.suptitle(title)
 
-def show_mean_images(images, labels, letters=None):
+
+def show_mean_images(images, labels, letters=None, title=None):
     """
     show_mean_images(images, c)
 
     Compute mean image for a cluster and show it.
 
-    :param labels:       image labels, np.array of size <number of images>; <label> is index to <Alphabet>
-    :param images:       images of letters, np.array of size <height x width x number of images>
-    :return:             mean of all images of the <letter_char>, uint8 type
+    :param images:          input images, np array (h, w, n)
+    :param labels:          labels of input images, np array (n, )
+    :param letters:         labels for mean images, string/array of chars
     """
     assert (len(images.shape) == 3)
 
@@ -308,7 +261,7 @@ def show_mean_images(images, labels, letters=None):
 
     unique_labels = np.unique(labels).flatten()
 
-    plt.figure()
+    fig = plt.figure()
     ww = np.ceil(float(n) / np.sqrt(n))
     hh = np.ceil(float(n) / ww)
 
@@ -319,18 +272,23 @@ def show_mean_images(images, labels, letters=None):
         plt.imshow(img_average, cmap='gray')
         if letters is not None:
             plt.title(letters[i])
+        plt.axis('off')
+
+    if title is not None:
+        fig.suptitle(title)
+
 
 
 def gen_kmeanspp_data(mu=None, sigma=None, n=None):
     """
-    samples = gen_kmeanspp_data()
+    generates data with n_clusterss normally distributed clusters
 
-    generates data with 4 normally distributed clusters
+    It generates 4 clusters with 80 points by default.
 
-    :param mu:      mean of normal distribution (np.array)
-    :param sigma:   std of normal distribution (scalar)
-    :param n:       number of output points for each distribution
-    :return:        samples - [2 x n*4] 2 dimensional samples with n samples per cluster
+    :param mu:          mean of normal distribution, np array (dim, n_clusters)
+    :param sigma:       std of normal distribution, scalar
+    :param n:           number of output points for each distribution, scalar
+    :return samples:    dim-dimensional samples with n samples per cluster, np array (dim, n_clusters * n)
     """
 
     sigma = 1. if sigma is None else sigma
